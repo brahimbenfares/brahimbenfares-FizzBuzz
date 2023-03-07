e3ddad3efcc484734e088dfbccdba71bb2d535f0
brahim benfares <br3mt2@gmail.com>
2023-03-07 01:01:09 +0000

    completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,4 +1,5 @@
 
+
 public class Lab {
     /**
      * Return "Fizz" if a number n is divisible by 3, "Buzz" if divisible by 5, and "FizzBuzz" if divisible by both.
@@ -8,13 +9,24 @@ public class Lab {
      * The modulus operator returns the remainder after a division (eg 8%3=2).
      *
      * If you try to copy answers from the internet, you will probably get this challenge wrong. The premise is
-     * slightly simpler than the classic fizzbuzz problem.
      *
      * @param n the number that will determine the fizzbuzz result.
      * @return if a number is divisible by 3, return 'Fizz'. if a number is divisible by 5, return 'Buzz'. If
-     * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
+     * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".     * slightly simpler than the classic fizzbuzz problem.
+
      */
     public String FizzBuzzGen(int n){
-        return "";
+
+         
+        if (n%3 ==0 && n%5==0)
+        {
+        return "FizzBuzz";}
+        else  if (n%3==0)
+        return "Fizz";
+        else if(n%5==0) 
+        return "Buzz";
+        else 
+        return"";
+
     }
 }
